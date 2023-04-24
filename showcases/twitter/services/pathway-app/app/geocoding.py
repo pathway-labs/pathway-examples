b56a87fd558959d4df72327662b5469a922cf4bf
Przemysław Uznański <przemek@pathway.com>
2023-04-24 10:16:23 +0200

    twitter cleanup (#3696)
    * simpler?
    
    * .
    
    * ixref
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Update public/pathway-examples/showcases/twitter/services/pathway-app/app/geocoding.py
    
    GitOrigin-RevId: 08347ca38e6592d4b5a12df7a7ad274260c23558

--- showcases/twitter/services/pathway-app/app/geocoding.py
+++ showcases/twitter/services/pathway-app/app/geocoding.py
@@ -88,35 +88,23 @@ def add_geolocation(tweet_pairs: pw.Table[TweetPairs]) -> pw.Table:
         geocoded=pw.apply_async(get_cached_geolocation_fun(), unique_locations.location)
     )
 
-    coord_from = (
-        tweet_pairs.join(
-            geocoded_locations,
-            tweet_pairs.tweet_from_author_location == geocoded_locations.location,
-            id=tweet_pairs.id,
-        )
-        .select(coord_from=geocoded_locations.geocoded)
-        .unsafe_promise_same_universe_as(tweet_pairs)
-    )
-    coord_to = (
-        tweet_pairs.join(
-            geocoded_locations,
-            tweet_pairs.tweet_to_author_location == geocoded_locations.location,
-            id=tweet_pairs.id,
-        )
-        .select(coord_to=geocoded_locations.geocoded)
-        .unsafe_promise_same_universe_as(tweet_pairs)
+    coord = tweet_pairs.select(
+        coord_from=geocoded_locations.ix_ref(
+            tweet_pairs.tweet_from_author_location
+        ).geocoded,
+        coord_to=geocoded_locations.ix_ref(
+            tweet_pairs.tweet_to_author_location
+        ).geocoded,
     )
 
     def is_good_location(geolocated) -> bool:
         return geolocated not in [e.value for e in GeolocatorInvalidFlags]
 
-    coord_from_is_good = coord_from.select(
-        is_good=pw.apply(is_good_location, coord_from.coord_from)
+    coord_is_good = coord.select(
+        from_is_good=pw.apply(is_good_location, coord.coord_from),
+        to_is_good=pw.apply(is_good_location, coord.coord_to),
     )
-    coord_to_is_good = coord_to.select(
-        is_good=pw.apply(is_good_location, coord_to.coord_to)
+    tweet_pair_is_good = coord_is_good.select(
+        is_good=coord_is_good.from_is_good & coord_is_good.to_is_good
     )
-    tweet_pair_is_good = tweet_pairs.select(
-        is_good=coord_from_is_good.is_good & coord_to_is_good.is_good
-    )
-    return tweet_pairs + coord_from + coord_to + tweet_pair_is_good
+    return tweet_pairs + coord + tweet_pair_is_good
